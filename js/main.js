d315942f445955e97d39197fb9417b053aef7e70
DenysmiK <129882672+DenysmiK@users.noreply.github.com>
2023-12-13 20:56:57 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,54 @@
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
+
+var items = document.querySelectorAll('.circle a');
+
+for(var i = 0, l = items.length; i < l; i++) {
+  items[i].style.left = (50 - 35*Math.cos(-0.5 * Math.PI - 2*(1/l)*i*Math.PI)).toFixed(4) + "%";
+  
+  items[i].style.top = (50 + 35*Math.sin(-0.5 * Math.PI - 2*(1/l)*i*Math.PI)).toFixed(4) + "%";
+}
+
+document.querySelector('.menu-button').onclick = function(e) {
+   e.preventDefault(); document.querySelector('.circle').classList.toggle('open');
+}
+
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+  event.preventDefault(); 
+
+  var name = document.getElementById('name').value;
+  var email = document.getElementById('email').value;
+  var message = document.getElementById('message').value;
+
+  if (name && email && message) {
+      document.getElementById('messageDisplay').textContent = 'Thank you for your message!';
+      document.getElementById('messageDisplay').style.color = 'green';
+  } else {
+      document.getElementById('messageDisplay').textContent = 'Please fill out all fields.';
+      document.getElementById('messageDisplay').style.color = 'red';
+  }
+});
